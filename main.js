02a183d8f5f44d40a8e320287b87298166bba301
Evan Jamaq <xevan19@gmail.com>
2024-07-09 12:57:42 +0700

    tambahin lagu

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 function playSong(id) {
-    // Pause all other songs
-    var audios = document.querySelectorAll('audio');
-    audios.forEach(audio => audio.pause());
+            // Pause semua lagu
+            var audios = document.querySelectorAll('audio');
+            audios.forEach(audio => audio.pause());
 
-    // Play selected song
-    var song = document.getElementById(id);
-    song.currentTime = 0;
-    song.play();
-}
-
-function pauseSong(id) {
-    var song = document.getElementById(id);
-    song.pause();
-}
-
-function filterSongs() {
-    var input, filter, songs, title, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toLowerCase();
-    songs = document.getElementsByClassName('lagu');
-
-    for (i = 0; i < songs.length; i++) {
-        title = songs[i].getElementsByClassName('lagu-title')[0];
-        txtValue = title.textContent || title.innerText;
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
-            songs[i].style.display = "";
-        } else {
-            songs[i].style.display = "none";
+            // Play lagu yang dipencet
+            var song = document.getElementById(id);
+            song.currentTime = 0;
+            song.play();
         }
-    }
-}
+
+        function pauseSong(id) {
+            var song = document.getElementById(id);
+            song.pause();
+        }
+
+        function filterSongs() {
+            var input, filter, songs, title, i, txtValue;
+            input = document.getElementById('searchInput');
+            filter = input.value.toLowerCase();
+            songs = document.getElementsByClassName('lagu');
+
+            for (i = 0; i < songs.length; i++) {
+                title = songs[i].getElementsByClassName('lagu-title')[0];
+                txtValue = title.textContent || title.innerText;
+                if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                    songs[i].style.display = "";
+                } else {
+                    songs[i].style.display = "none";
+                }
+            }
+        }
